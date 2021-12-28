44ed86af78f64f378ef53505b84e53220aaee64b
Michael Wu <mw10013@gmail.com>
2021-12-28 13:33:26 -0500

    heartbeats

--- app/routes/heartbeat/$key.tsx
+++ app/routes/heartbeat/$key.tsx
@@ -1,11 +1,23 @@
 import type { LoaderFunction } from "remix";
 import { json } from "remix";
-import type { AccessPoint } from "@prisma/client";
+import { AccessPoint, prisma } from "@prisma/client";
 import { db } from "~/utils/db.server";
 
 //https://remix.run/docs/en/v1.0.6/api/remix#json
 
 export const loader: LoaderFunction = async ({ params }) => {
-  const accessPoints = await db.accessPoint.findMany();
-  return json({ key: params.key, accessPoints, success: true }, 200);
+  const accessPoint = await db.accessPoint.findUnique({
+    where: { key: params.key },
+  });
+  if (!accessPoint) {
+    throw new Response("Key not found.", {
+      status: 404,
+    });
+  }
+  const updatedAccessPoint = await db.accessPoint.update({
+    where: { id: accessPoint.id },
+    data: { heartbeatAt: new Date(), heartbeats: { increment: 1}}
+  });
+
+  return json({ accessPoint: updatedAccessPoint }, 200);
 };
