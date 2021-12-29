71d7e6124a8f9616714c6e18f31db51c2ece9ac0
Michael Wu <mw10013@gmail.com>
2021-12-28 21:05:27 -0500

    access panel

--- app/routes/mock/$key.tsx
+++ app/routes/mock/$key.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import type { LoaderFunction } from "remix";
 import { useLoaderData } from "remix";
 import type { AccessPoint } from "@prisma/client";
@@ -20,5 +21,40 @@ export const loader: LoaderFunction = async ({ params: { key } }) => {
 
 export default function MockRoute() {
   const { accessPoint } = useLoaderData<LoaderData>();
-  return <div>{accessPoint.key}</div>;
+  const [code, setCode] = React.useState("");
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold leading-7 text-gray-900">
+        Mock <span className="text-md text-gray-400">{accessPoint.key}</span>
+      </h1>
+
+      <div className="m-4 bg-white shadow sm:rounded-lg">
+        <div className="px-4 py-5 sm:p-6">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Code</h3>
+          <form className="mt-1 sm:flex sm:items-center">
+            <div className="w-full sm:max-w-xs">
+              <label htmlFor="code" className="sr-only">
+                Code
+              </label>
+              <input
+                type="text"
+                name="code"
+                id="code"
+                className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                value={code}
+                onChange={(e) => setCode(e.target.value)}
+              />
+            </div>
+            <button
+              type="button"
+              className="mt-3 w-full inline-flex items-center justify-center px-4 py-2 border border-transparent shadow-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+              onClick={() => console.log({ code })}
+            >
+              Access
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
