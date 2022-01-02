f8c43d908912830f2aa87b8441e50e150b1151e8
Michael Wu <mw10013@gmail.com>
2022-01-02 15:03:15 -0500

    schema for codes

--- app/routes/accesspoint/$id/raw.tsx
+++ app/routes/accesspoint/$id/raw.tsx
@@ -6,14 +6,14 @@ import { db } from "~/utils/db.server";
 
 type LoaderData = {
   accessPoint: Prisma.AccessPointGetPayload<{
-    include: { cachedConfig: true };
+    include: { codes: true; cachedConfig: true };
   }>;
 };
 
 export const loader: LoaderFunction = async ({ params: { id } }) => {
   const accessPoint = await db.accessPoint.findUnique({
     where: { id: Number(id) },
-    include: { cachedConfig: true },
+    include: { codes: true, cachedConfig: true },
   });
   if (!accessPoint) {
     throw new Response("Access point not found.", {
@@ -26,19 +26,43 @@ export const loader: LoaderFunction = async ({ params: { id } }) => {
 
 export default function RawlRoute() {
   const data = useLoaderData<LoaderData>();
+  const [poll, setPoll] = React.useState(true);
   const navigate = useNavigate();
 
   React.useEffect(() => {
-    const intervalId = setInterval(
-      () => navigate(".", { replace: true }),
-      5000
-    );
-    return () => clearInterval(intervalId);
-  }, [navigate]);
+    if (poll) {
+      const intervalId = setInterval(
+        () => navigate(".", { replace: true }),
+        5000
+      );
+      return () => clearInterval(intervalId);
+    }
+  }, [navigate, poll]);
 
   return (
     <div className="p-8">
-      <h1 className="text-2xl font-bold leading-7 text-gray-900">Raw</h1>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold leading-7 text-gray-900">Raw</h1>
+        <div className="relative flex items-start">
+          <div className="flex items-center h-5">
+            <input
+              id="poll"
+              aria-describedby="comments-description"
+              name="poll"
+              type="checkbox"
+              checked={poll}
+              onClick={() => setPoll(!poll)}
+              className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+            />
+          </div>
+          <div className="ml-3 text-sm">
+            <label htmlFor="poll" className="font-medium text-gray-700">
+              Poll
+            </label>
+          </div>
+        </div>
+      </div>
+
       <pre className="mt-4">{JSON.stringify(data, null, 2)}</pre>
     </div>
   );
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,6 +7,14 @@ async function seed() {
       data: {
         key: "key1",
         code: "13795",
+        codes: {
+          create: [
+            { name: "guest1", code: "111", enabled: true },
+            { name: "guest2", code: "111", enabled: false },
+            { name: "staff", code: "333", enabled: true },
+            { name: "master", code: "444", enabled: true },
+          ],
+        },
         accessCheckPolicy: "cloud-first",
         heartbeatAt: new Date(),
         cachedConfig: {
