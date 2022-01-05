7a5b21cf7116952990219f8937bcdfbdc4657749
Michael Wu <mw10013@gmail.com>
2022-01-05 15:12:06 -0500

    add accesspoint to user

---
+++ app/routes/users/$accessUserId/accesspoints.add.tsx
@@ -0,0 +1,121 @@
+import * as React from "react";
+import type { ActionFunction, LoaderFunction } from "remix";
+import {
+  useActionData,
+  useLoaderData,
+  Form,
+  useNavigate,
+  redirect,
+} from "remix";
+import { Prisma } from "@prisma/client";
+import { db } from "~/utils/db.server";
+
+type LoaderData = {
+  accessUser: Prisma.AccessUserGetPayload<{
+    include: { accessPoints: true };
+  }>;
+  accessPoints: Prisma.AccessPointGetPayload<{}>[];
+};
+
+export const loader: LoaderFunction = async ({
+  params: { accessUserId: id },
+}): Promise<LoaderData> => {
+  const accessUser = await db.accessUser.findUnique({
+    where: { id: Number(id) },
+    include: { accessPoints: true },
+    rejectOnNotFound: true,
+  });
+  const notIn = accessUser.accessPoints.map((el) => el.id);
+  const accessPoints = await db.accessPoint.findMany({
+    where: { id: { notIn } },
+  });
+  return { accessUser, accessPoints };
+};
+
+export const action: ActionFunction = async ({
+  request,
+  params: { accessUserId },
+}) => {
+  const formData = await request.formData();
+  // Node FormData get() seems to return null for empty string value.
+  // Object.fromEntries(formData): if formData.entries() has 2 entries with the same key, only 1 is taken.
+  const fieldValues = Object.fromEntries(formData);
+
+  let ids = [];
+  for (let idx = 0; fieldValues[`accessPoint-${idx}-id`]; ++idx) {
+    if (fieldValues[`accessPoint-${idx}`]) {
+      ids.push(Number(fieldValues[`accessPoint-${idx}-id`]));
+    }
+  }
+  if (ids.length > 0) {
+    await db.accessUser.update({
+      where: { id: Number(accessUserId) },
+      data: { accessPoints: { connect: ids.map((id) => ({ id })) } },
+    });
+  }
+  return redirect(`/users/${accessUserId}`);
+};
+
+export default function Add() {
+  const { accessUser, accessPoints } = useLoaderData<LoaderData>();
+  const navigate = useNavigate();
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold leading-7 text-gray-900">
+        Add Access Points{accessUser.name ? ` to ${accessUser.name}` : null}
+      </h1>
+      <Form method="post" className="mt-4">
+        <fieldset>
+          <legend className="text-lg font-medium text-gray-900">
+            Available Access Points
+          </legend>
+          <div className="mt-4 border-t border-b border-gray-200 divide-y divide-gray-200">
+            {accessPoints.map((ap, apIdx) => (
+              <div key={ap.id} className="relative flex items-start py-4">
+                <div className="min-w-0 flex-1 text-sm">
+                  <label
+                    htmlFor={`accessPoint-${apIdx}`}
+                    className="font-medium text-gray-700 select-none"
+                  >
+                    {`ID: ${ap.id}: ${ap.name}`}
+                  </label>
+                </div>
+                <div className="ml-3 flex items-center h-5">
+                  <input
+                    id={`accessPoint-${apIdx}`}
+                    name={`accessPoint-${apIdx}`}
+                    type="checkbox"
+                    className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
+                  />
+                  <input
+                    id={`accessPoint-${apIdx}-id`}
+                    name={`accessPoint-${apIdx}-id`}
+                    type="hidden"
+                    value={ap.id}
+                  />
+                </div>
+              </div>
+            ))}
+          </div>
+        </fieldset>
+        <div className="pt-5">
+          <div className="flex justify-end">
+            <button
+              type="button"
+              className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              onClick={() => navigate(`/users/${accessUser.id}`)}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Add
+            </button>
+          </div>
+        </div>
+      </Form>
+    </div>
+  );
+}
--- app/routes/users/$accessUserId/index.tsx
+++ app/routes/users/$accessUserId/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import type { LoaderFunction } from "remix";
-import { useLoaderData, Link } from "remix";
+import { useLoaderData, Link, useNavigate } from "remix";
 // import type { AccessPoint } from "@prisma/client";
 import { Prisma } from "@prisma/client";
 import { db } from "~/utils/db.server";
@@ -23,6 +23,7 @@ export const loader: LoaderFunction = async ({
 };
 
 export default function Index() {
+  const navigate = useNavigate();
   const { accessUser } = useLoaderData<LoaderData>();
   return (
     <div className="p-8">
@@ -45,6 +46,82 @@ export default function Index() {
           )}
         </div>
       </div>
+
+      {accessUser.description ? (
+        <p className="mt-4">{accessUser.description}</p>
+      ) : null}
+
+      <div className="mt-4">
+        <div className="flex justify-between">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">
+            Access Points
+          </h3>
+          <button
+            type="button"
+            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
+            onClick={() => navigate("./accesspoints/add")}
+          >
+            Add
+          </button>
+        </div>
+        <div className="mt-2 max-w-xl text-sm text-gray-500">
+          <p></p>
+        </div>
+        <div className="max-w-7xl mx-auto">
+          <table className="mt-4 max-width-md divide-y divide-gray-200">
+            <thead className="bg-gray-50">
+              <tr>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  ID
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Name
+                </th>
+                <th
+                  scope="col"
+                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                >
+                  Description
+                </th>
+                <th scope="col" className="relative px-6 py-3">
+                  <span className="sr-only">View</span>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {accessUser.accessPoints.map((ap) => (
+                <tr key={ap.id}>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-500">
+                    {ap.id}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                    {ap.name}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                    {ap.description}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                    <Link
+                      to={`accessPoints/${ap.id}`}
+                      className="text-indigo-600 hover:text-indigo-900"
+                    >
+                      View
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+      <pre className="mt-2">{JSON.stringify(accessUser, null, 2)}</pre>
     </div>
   );
 }
