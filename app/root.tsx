29f0c357878aa8465739aedd9b62d8af006a7398
Michael Wu <mw10013@gmail.com>
2022-01-05 11:08:58 -0500

    navigation

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,16 @@ import {
 import type { MetaFunction } from "remix";
 import styles from "./tailwind.css";
 
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const navigation = [
+  { name: "Dashboard", href: "/", current: true },
+  { name: "Users", href: "/users", current: false },
+  { name: "Access Points", href: "accessPoints", current: false },
+];
+
 export const meta: MetaFunction = () => {
   return { title: "Access" };
 };
@@ -29,10 +39,33 @@ export default function App() {
       </head>
       <body>
         <nav className="bg-white border-b border-gray-200">
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-16 flex items-center">
-            <h1 className="text-2xl font-bold leading-7 text-gray-900">
-              <Link to=".">Access</Link>
-            </h1>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            <div className="flex justify-between h-16">
+              <div className="flex">
+                <div className="flex-shrink-0 flex items-center">
+                  <h1 className="text-2xl font-bold leading-7 text-gray-900">
+                    <Link to=".">Access</Link>
+                  </h1>
+                </div>
+                <div className="-my-px ml-6 flex space-x-8">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      to={item.href}
+                      className={classNames(
+                        item.current
+                          ? "border-indigo-500 text-gray-900"
+                          : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                        "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
         </nav>
         <Outlet />
