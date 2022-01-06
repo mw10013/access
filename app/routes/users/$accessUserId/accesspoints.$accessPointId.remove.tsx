82ddba232250077633cdea8125b3679157f70774
Michael Wu <mw10013@gmail.com>
2022-01-06 09:19:00 -0500

    create user and unique constraint on name and code

--- app/routes/users/$accessUserId/accesspoints.$accessPointId.remove.tsx
+++ app/routes/users/$accessUserId/accesspoints.$accessPointId.remove.tsx
@@ -1,21 +1,10 @@
-import * as React from "react";
 import type { ActionFunction, LoaderFunction } from "remix";
-import {
-  useActionData,
-  useLoaderData,
-  Form,
-  useNavigate,
-  redirect,
-} from "remix";
-import { Prisma } from "@prisma/client";
+import { redirect } from "remix";
 import { db } from "~/utils/db.server";
 
 export const action: ActionFunction = async ({
-  request,
   params: { accessUserId, accessPointId },
 }) => {
-  console.log({ fn: "remove", accessUserId, accessPointId });
-
   await db.accessUser.update({
     where: { id: Number(accessUserId) },
     data: {
--- app/routes/users/index.tsx
+++ app/routes/users/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import type { LoaderFunction } from "remix";
-import { useLoaderData, Link } from "remix";
+import { useLoaderData, Link, useNavigate } from "remix";
 // import type { AccessPoint } from "@prisma/client";
 import { Prisma } from "@prisma/client";
 import { db } from "~/utils/db.server";
@@ -21,9 +21,19 @@ export const loader: LoaderFunction = async (): Promise<LoaderData> => {
 
 export default function Index() {
   const { accessUsers } = useLoaderData<LoaderData>();
+  const navigate = useNavigate();
   return (
     <div className="p-8">
-      <h1 className="text-2xl font-bold leading-7 text-gray-900">Users</h1>
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold leading-7 text-gray-900">Users</h1>
+        <button
+          type="button"
+          className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
+          onClick={() => navigate("create")}
+        >
+          Create
+        </button>
+      </div>
       <table className="mt-4 max-width-md divide-y divide-gray-200">
         <thead className="bg-gray-50">
           <tr>
