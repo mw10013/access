de644578477fd1ff37df6b0381bf6f348ff30e68
Michael Wu <mw10013@gmail.com>
2022-01-06 11:23:29 -0500

    links to mock and raw

--- app/routes/accesspoints/$accessPointId/index.tsx
+++ app/routes/accesspoints/$accessPointId/index.tsx
@@ -28,7 +28,7 @@ export default function Index() {
     <div className="p-8">
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold leading-7 text-gray-900">
-          Access Point {accessPoint.name}
+          Access Point: {accessPoint.name}
         </h1>
         <button
           type="button"
@@ -40,11 +40,8 @@ export default function Index() {
       </div>
       <div className="flex mt-1 space-x-10 text-sm text-gray-500">
         <div>ID: {accessPoint.id}</div>
+        <div>{accessPoint.description}</div>
       </div>
-
-      {accessPoint.description ? (
-        <p className="mt-4">{accessPoint.description}</p>
-      ) : null}
 
       <div className="mt-4">
         <div className="flex justify-between">
@@ -138,6 +135,23 @@ export default function Index() {
             </tbody>
           </table>
         </div>
+      </div>
+
+      <div className="mt-4 flex space-x-2">
+        <button
+          type="button"
+          className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
+          onClick={() => navigate("mock")}
+        >
+          Mock
+        </button>
+        <button
+          type="button"
+          className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
+          onClick={() => navigate("raw")}
+        >
+          Raw
+        </button>
       </div>
 
       {/* <pre className="mt-2">{JSON.stringify(accessUser, null, 2)}</pre> */}
