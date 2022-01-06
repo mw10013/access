1bcefbc638f71245b4e57ce110c6a929cf6a6db0
Michael Wu <mw10013@gmail.com>
2022-01-06 11:30:54 -0500

    edit access point

--- app/routes/accesspoints/$accessPointId/edit.tsx
+++ app/routes/accesspoints/$accessPointId/edit.tsx
@@ -1,18 +1,18 @@
 import type { ActionFunction, LoaderFunction } from "remix";
 import { useActionData, useLoaderData, Form, useSubmit, redirect } from "remix";
-import type { AccessUser } from "@prisma/client";
+import type { AccessPoint } from "@prisma/client";
 import { db } from "~/utils/db.server";
 
-type LoaderData = { accessUser: AccessUser };
+type LoaderData = { accessPoint: AccessPoint };
 
 export const loader: LoaderFunction = async ({
-  params: { accessUserId: id },
+  params: { accessPointId },
 }): Promise<LoaderData> => {
-  const accessUser = await db.accessUser.findUnique({
-    where: { id: Number(id) },
+  const accessPoint = await db.accessPoint.findUnique({
+    where: { id: Number(accessPointId) },
     rejectOnNotFound: true,
   });
-  return { accessUser };
+  return { accessPoint };
 };
 
 function validateName(name: string) {
@@ -30,80 +30,61 @@ function validateDescription(description: string) {
   }
 }
 
-function validateCode(code: string) {
-  if (code.length === 0) {
-    return "Code is required.";
-  }
-  if (!/^\d+$/.test(code)) {
-    return "Code must contain only digits.";
-  }
-  if (code.length < 3) {
-    return "Code must have at least 3 digits";
-  }
-  if (code.length > 8) {
-    return "Code must have no more than 8 digits.";
-  }
-}
-
 type ActionData = {
   formError?: string;
   fieldErrors?: {
     name?: string | undefined;
     description?: string | undefined;
-    code?: string | undefined;
-    // enabled?: string | undefined;
   };
   fieldValues?: any;
 };
 
 export const action: ActionFunction = async ({
   request,
-  params: { accessUserId },
+  params: { accessPointId },
 }): Promise<Response | ActionData> => {
+  /*    
   if (request.method === "DELETE") {
-    await db.accessUser.delete({
-      where: { id: Number(accessUserId) },
+    await db.accessPoint.delete({
+      where: { id: Number(accessPointId) },
     });
     return redirect("/users");
   }
-
+*/
   const formData = await request.formData();
   // Node FormData get() seems to return null for empty string value.
   // Object.fromEntries(formData): if formData.entries() has 2 entries with the same key, only 1 is taken.
   const fieldValues = Object.fromEntries(formData);
-  const { name, description, code, enabled } = fieldValues;
-  if (
-    typeof name !== "string" ||
-    typeof description !== "string" ||
-    typeof code !== "string"
-  ) {
+  const { name, description } = fieldValues;
+  if (typeof name !== "string" || typeof description !== "string") {
     return { formError: `Form not submitted correctly.` };
   }
 
   const fieldErrors = {
     name: validateName(name),
     description: validateDescription(description),
-    code: validateCode(code),
   };
   if (Object.values(fieldErrors).some(Boolean)) {
     return { fieldErrors, fieldValues };
   }
 
-  await db.accessUser.update({
-    where: { id: Number(accessUserId) },
-    data: { name, description, code, enabled: !!enabled },
+  await db.accessPoint.update({
+    where: { id: Number(accessPointId) },
+    data: { name, description },
   });
 
-  return redirect(`/users/${accessUserId}`);
+  return redirect(`/accesspoints/${accessPointId}`);
 };
 
 export default function Edit() {
-  const { accessUser } = useLoaderData<LoaderData>();
+  const { accessPoint } = useLoaderData<LoaderData>();
   const actionData = useActionData<ActionData>();
   const submit = useSubmit();
   return (
     <div className="p-8">
-      <h1 className="text-2xl font-bold leading-7 text-gray-900">Edit User</h1>
+      <h1 className="text-2xl font-bold leading-7 text-gray-900">
+        Edit Access Point
+      </h1>
       <Form replace method="post">
         <div>
           <h3 className="text-lg leading-6 font-medium text-gray-900">
@@ -129,7 +110,7 @@ export default function Edit() {
                 defaultValue={
                   actionData?.fieldValues
                     ? actionData.fieldValues.name
-                    : accessUser.name
+                    : accessPoint.name
                 }
                 className="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-md sm:text-sm border-gray-300"
               />
@@ -163,7 +144,7 @@ export default function Edit() {
                 defaultValue={
                   actionData?.fieldValues
                     ? actionData.fieldValues.description
-                    : accessUser.description
+                    : accessPoint.description
                 }
                 className="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-md sm:text-sm border-gray-300"
               />
@@ -180,72 +161,7 @@ export default function Edit() {
           </div>
         </div>
 
-        <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-          <div className="sm:col-span-4">
-            <label
-              htmlFor="code"
-              className="block text-sm font-medium text-gray-700"
-            >
-              Code
-            </label>
-
-            <div className="mt-1 flex rounded-md shadow-sm">
-              <input
-                type="text"
-                name="code"
-                id="code"
-                defaultValue={
-                  actionData?.fieldValues
-                    ? actionData.fieldValues.code
-                    : accessUser.code
-                }
-                className="flex-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full min-w-0 rounded-md sm:text-sm border-gray-300"
-              />
-            </div>
-            {actionData?.fieldErrors?.code ? (
-              <p
-                className="mt-2 text-sm text-red-600"
-                role="alert"
-                id="code-error"
-              >
-                {actionData.fieldErrors.code}
-              </p>
-            ) : null}
-          </div>
-        </div>
-
-        <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-          <div className="relative flex items-start">
-            <div className="flex items-center h-5">
-              <input
-                id="enabled"
-                name="enabled"
-                type="checkbox"
-                defaultChecked={
-                  actionData?.fieldValues
-                    ? actionData.fieldValues.enabled
-                    : accessUser.enabled
-                }
-                className="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded"
-              />
-            </div>
-            <div className="ml-3 text-sm">
-              <label htmlFor="enabled" className="font-medium text-gray-700">
-                Enabled
-              </label>
-            </div>
-          </div>
-        </div>
-
         <div className="mt-4 flex justify-between">
-          <button
-            type="button"
-            onClick={(e) => submit(e.currentTarget.form, { method: "delete" })}
-            className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-rose-600 hover:bg-rose-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
-          >
-            Delete
-          </button>
-
           <button
             type="submit"
             className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
