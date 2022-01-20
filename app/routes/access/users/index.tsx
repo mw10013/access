a933a4767651835d8c0e5c58daa92c4a54004165
Michael Wu <mw10013@gmail.com>
2022-01-20 09:44:37 -0500

    removed enabled

--- app/routes/access/users/index.tsx
+++ app/routes/access/users/index.tsx
@@ -54,12 +54,6 @@ export default function Index() {
             >
               Code
             </th>
-            <th
-              scope="col"
-              className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-            >
-              Enabled
-            </th>
             <th scope="col" className="relative px-6 py-3">
               <span className="sr-only">View</span>
             </th>
@@ -76,9 +70,6 @@ export default function Index() {
               </td>
               <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                 {i.code}
-              </td>
-              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                {i.enabled ? "Enabled" : "Disabled"}
               </td>
               <td className="px-6 py-4 whitespace-nowrap text-right text-sm">
                 <Link
