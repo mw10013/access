c31581aa5b7a2132105318608492424c9c64c0b2
Michael Wu <mw10013@gmail.com>
2022-01-22 15:40:13 -0500

    styles

--- app/routes/access/managers/$accessManagerId/index.tsx
+++ app/routes/access/managers/$accessManagerId/index.tsx
@@ -1,8 +1,47 @@
+import { Fragment } from "react";
+import {
+  BriefcaseIcon,
+  CalendarIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+  CurrencyDollarIcon,
+  LinkIcon,
+  LocationMarkerIcon,
+  PencilIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/solid";
+import { Menu, Transition } from "@headlessui/react";
 import type { LoaderFunction } from "remix";
 import { useLoaderData, Link, useNavigate, useSubmit } from "remix";
 import { Prisma } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { requireUserId } from "~/utils/session.server";
+import { Table, Th } from "~/components/lib";
+
+const payments = [
+  {
+    id: 1,
+    date: "1/1/2020",
+    datetime: "2020-01-01",
+    description: "Business Plan - Annual Billing",
+    amount: "CA$109.00",
+    href: "#",
+  },
+  {
+    id: 2,
+    date: "1/7/2020",
+    datetime: "2020-01-01",
+    description: "Business Plan ",
+    amount: "CA$10.00",
+    href: "#",
+  },
+  // More payments...
+];
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
+}
 
 type LoaderData = {
   accessManager: Prisma.AccessManagerGetPayload<{
@@ -29,119 +68,230 @@ export const loader: LoaderFunction = async ({
   return { accessManager };
 };
 
-export default function Index() {
+export default function RouteComponent() {
   const navigate = useNavigate();
   const { accessManager } = useLoaderData<LoaderData>();
   return (
-    <div className="p-8">
-      <div className="flex justify-between">
-        <h1 className="text-2xl font-bold leading-7 text-gray-900">
-          Access Manager
-        </h1>
-        <div className="flex space-x-2">
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
-            onClick={() => navigate("mock")}
-          >
-            Mock
-          </button>
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
-            onClick={() => navigate("raw")}
-          >
-            Raw
-          </button>
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
-            onClick={() => navigate("activity")}
-          >
-            Activity
-          </button>
-          <button
-            type="button"
-            className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-50 focus:ring-purple-500"
-            onClick={() => navigate("edit")}
-          >
-            Edit
-          </button>
-        </div>
-      </div>
-      <div className="flex mt-1 space-x-10 text-sm text-gray-500">
-        <div>{accessManager.name}</div>
-        <div>ID: {accessManager.id}</div>
-      </div>
-
-      {accessManager.description ? (
-        <p className="mt-2 text-sm text-gray-500">
-          {accessManager.description}
-        </p>
-      ) : null}
-
-      <div className="mt-4">
-        <div className="flex justify-between">
-          <h3 className="text-lg leading-6 font-medium text-gray-900">
-            Access Points
-          </h3>
-        </div>
-        <div className="mt-2 max-w-xl text-sm text-gray-500">
-          <p></p>
-        </div>
-        <div className="max-w-7xl mx-auto">
-          <table className="mt-4 max-width-md divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Position
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Name
-                </th>
-                <th
-                  scope="col"
-                  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                >
-                  Description
-                </th>
-                <th scope="col" className="relative px-6 py-3">
-                  <span className="sr-only">View</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {accessManager.accessPoints.map((i) => (
-                <tr key={i.id}>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {i.position}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm font-medium  text-gray-900">
-                    {i.name}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                    {i.description}
-                  </td>
-                  <td className="px-6 py-4 whitespace-nowrap text-right text-sm">
+    <>
+      <header className="p-8">
+        <div className="lg:flex lg:items-center lg:justify-between">
+          <div className="flex-1 min-w-0">
+            <nav className="flex" aria-label="Breadcrumb">
+              <ol role="list" className="flex items-center space-x-4">
+                <li>
+                  <div className="flex">
                     <Link
-                      to={`../points/${i.id}`}
-                      className="text-indigo-600 hover:text-indigo-900"
+                      to="./.."
+                      className="text-sm font-medium text-gray-500 hover:text-gray-700"
                     >
-                      View
+                      Managers
                     </Link>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+                  </div>
+                </li>
+                <li>
+                  <div className="flex items-center">
+                    <ChevronRightIcon
+                      className="flex-shrink-0 h-5 w-5 text-gray-400"
+                      aria-hidden="true"
+                    />
+                    <Link
+                      to="."
+                      className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                    >
+                      Access Manager
+                    </Link>
+                  </div>
+                </li>
+              </ol>
+            </nav>
+            <h2 className="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
+              {accessManager.name}
+            </h2>
+            <div className="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
+              <div className="mt-2 flex items-center text-sm text-gray-500">
+                <LocationMarkerIcon
+                  className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                {accessManager.description}
+              </div>
+              {/* <div className="mt-2 flex items-center text-sm text-gray-500">
+                <LocationMarkerIcon
+                  className="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400"
+                  aria-hidden="true"
+                />
+                Remote
+              </div> */}
+            </div>
+          </div>
+
+          <div className="mt-5 flex lg:mt-0 lg:ml-4">
+            <span className="hidden sm:block">
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={() => navigate("mock")}
+              >
+                <PencilIcon
+                  className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                  aria-hidden="true"
+                />
+                Mock
+              </button>
+            </span>
+
+            <span className="hidden sm:block ml-3">
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={() => navigate("raw")}
+              >
+                <CheckIcon
+                  className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                  aria-hidden="true"
+                />
+                Raw
+              </button>
+            </span>
+            <span className="hidden sm:block ml-3">
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={() => navigate("activity")}
+              >
+                <LinkIcon
+                  className="-ml-1 mr-2 h-5 w-5 text-gray-500"
+                  aria-hidden="true"
+                />
+                Activity
+              </button>
+            </span>
+            <span className="sm:ml-3">
+              <button
+                type="button"
+                className="inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                onClick={() => navigate("edit")}
+              >
+                <PencilIcon className="-ml-1 mr-2 h-5 w-5" aria-hidden="true" />
+                Edit
+              </button>
+            </span>
+
+            {/* Dropdown */}
+            <Menu as="span" className="ml-3 relative sm:hidden">
+              <Menu.Button className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                More
+                <ChevronDownIcon
+                  className="-mr-1 ml-2 h-5 w-5 text-gray-500"
+                  aria-hidden="true"
+                />
+              </Menu.Button>
+
+              <Transition
+                as={Fragment}
+                enter="transition ease-out duration-200"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+              >
+                <Menu.Items className="origin-top-right absolute right-0 mt-2 -mr-1 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="mock"
+                        className={classNames(
+                          active ? "bg-gray-100" : "",
+                          "block px-4 py-2 text-sm text-gray-700"
+                        )}
+                      >
+                        Mock
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="raw"
+                        className={classNames(
+                          active ? "bg-gray-100" : "",
+                          "block px-4 py-2 text-sm text-gray-700"
+                        )}
+                      >
+                        Raw
+                      </Link>
+                    )}
+                  </Menu.Item>
+                  <Menu.Item>
+                    {({ active }) => (
+                      <Link
+                        to="activity"
+                        className={classNames(
+                          active ? "bg-gray-100" : "",
+                          "block px-4 py-2 text-sm text-gray-700"
+                        )}
+                      >
+                        Activity
+                      </Link>
+                    )}
+                  </Menu.Item>
+                </Menu.Items>
+              </Transition>
+            </Menu>
+          </div>
         </div>
-      </div>
-    </div>
+      </header>
+      <main className="max-w-7xl mx-auto sm:px-8 space-y-6 pb-8">
+        <section aria-labelledby="access-points-heading">
+          <div className="bg-white pt-6 shadow sm:rounded-md sm:overflow-hidden">
+            <div className="px-4 sm:px-6">
+              <h2
+                id="access-points-heading"
+                className="text-lg leading-6 font-medium text-gray-900"
+              >
+                Access Points
+              </h2>
+            </div>
+            <div className="mt-6">
+              <Table
+                headers={
+                  <>
+                    <Th>Position</Th>
+                    <Th>Name</Th>
+                    <Th>Description</Th>
+                    <th scope="col" className="relative px-6 py-3">
+                      <span className="sr-only">View</span>
+                    </th>
+                  </>
+                }
+              >
+                {accessManager.accessPoints.map((i) => (
+                  <tr key={i.id}>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {i.position}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                      {i.name}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {i.description}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <Link
+                        to={`../points/${i.id}`}
+                        className="text-indigo-600 hover:text-indigo-900"
+                      >
+                        View
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </Table>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
   );
 }
