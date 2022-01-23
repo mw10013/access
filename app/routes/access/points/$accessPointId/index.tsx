7180415606ef425080f3f5219138e63b5a0810d4
Michael Wu <mw10013@gmail.com>
2022-01-23 17:30:16 -0500

    buttons

--- app/routes/access/points/$accessPointId/index.tsx
+++ app/routes/access/points/$accessPointId/index.tsx
@@ -73,7 +73,7 @@ export default function RouteComponent() {
           </div>
 
           <div className="mt-5 flex lg:mt-0 lg:ml-4">
-            <span className="hidden sm:block ml-3">
+            <span className="hidden sm:block">
               <button
                 type="button"
                 className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
