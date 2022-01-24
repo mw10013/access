de06bf2e6d0f961bcb1ec867fcfcc586af98b399
Michael Wu <mw10013@gmail.com>
2022-01-24 09:25:50 -0500

    focus

--- app/routes/access/managers/$accessManagerId/edit.tsx
+++ app/routes/access/managers/$accessManagerId/edit.tsx
@@ -109,7 +109,7 @@ export default function RouteComponent() {
                     type="text"
                     name="name"
                     id="name"
-                    className="block w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border-gray-300 rounded-md"
+                    className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border-gray-300 rounded-md"
                     defaultValue={
                       actionData?.fieldValues
                         ? actionData.fieldValues.name
@@ -140,7 +140,7 @@ export default function RouteComponent() {
                     id="description"
                     name="description"
                     rows={3}
-                    className="block w-full shadow-sm focus:ring-sky-500 focus:border-sky-500 sm:text-sm border border-gray-300 rounded-md"
+                    className="block w-full shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm border border-gray-300 rounded-md"
                     defaultValue={
                       actionData?.fieldValues
                         ? actionData.fieldValues.description
