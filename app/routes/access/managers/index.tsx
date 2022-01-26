279d60a94c37b9fef166e4b3d7945c9eb5fc9f59
Michael Wu <mw10013@gmail.com>
2022-01-26 14:26:42 -0500

    header and main

--- app/routes/access/managers/index.tsx
+++ app/routes/access/managers/index.tsx
@@ -3,7 +3,16 @@ import { useLoaderData } from "remix";
 import { Prisma } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { requireUserId } from "~/utils/session.server";
-import { Table, Td, TdLink, TdProminent, Th, ThSr } from "~/components/lib";
+import {
+  Header,
+  Main,
+  Table,
+  Td,
+  TdLink,
+  TdProminent,
+  Th,
+  ThSr,
+} from "~/components/lib";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -30,39 +39,28 @@ export default function RouteComponent() {
   const { accessManagers } = useLoaderData<LoaderData>();
   return (
     <>
-      <header className="p-8">
-        <div className="lg:flex lg:items-center lg:justify-between">
-          <div className="flex-1 min-w-0">
-            <h2 className="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-              Managers
-            </h2>
-          </div>
-        </div>
-      </header>
-
-      <main className="">
-        <div className="max-w-7xl mx-auto sm:px-8">
-          <Table
-            headers={
-              <>
-                <Th>Name</Th>
-                <Th>Id</Th>
-                <Th>Description</Th>
-                <ThSr>View</ThSr>
-              </>
-            }
-          >
-            {accessManagers.map((i) => (
-              <tr key={i.id}>
-                <TdProminent>{i.name}</TdProminent>
-                <Td>{i.id}</Td>
-                <Td>{i.description}</Td>
-                <TdLink to={`${i.id}`}>View</TdLink>
-              </tr>
-            ))}
-          </Table>
-        </div>
-      </main>
+      <Header title="Managers" />
+      <Main>
+        <Table
+          headers={
+            <>
+              <Th>Name</Th>
+              <Th>Id</Th>
+              <Th>Description</Th>
+              <ThSr>View</ThSr>
+            </>
+          }
+        >
+          {accessManagers.map((i) => (
+            <tr key={i.id}>
+              <TdProminent>{i.name}</TdProminent>
+              <Td>{i.id}</Td>
+              <Td>{i.description}</Td>
+              <TdLink to={`${i.id}`}>View</TdLink>
+            </tr>
+          ))}
+        </Table>
+      </Main>
     </>
   );
 }
--- app/routes/access/points/index.tsx
+++ app/routes/access/points/index.tsx
@@ -3,7 +3,16 @@ import { useLoaderData, Link } from "remix";
 import { Prisma } from "@prisma/client";
 import { db } from "~/utils/db.server";
 import { requireUserId } from "~/utils/session.server";
-import { Table, Td, TdLink, TdProminent, Th, ThSr } from "~/components/lib";
+import {
+  Header,
+  Main,
+  Table,
+  Td,
+  TdLink,
+  TdProminent,
+  Th,
+  ThSr,
+} from "~/components/lib";
 
 type LoaderData = {
   accessPoints: Prisma.AccessPointGetPayload<{
@@ -33,39 +42,28 @@ export default function RouteComponent() {
   const { accessPoints } = useLoaderData<LoaderData>();
   return (
     <>
-      <header className="p-8">
-        <div className="lg:flex lg:items-center lg:justify-between">
-          <div className="flex-1 min-w-0">
-            <h2 className="mt-2 text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-              Points
-            </h2>
-          </div>
-        </div>
-      </header>
-
-      <main>
-        <div className="max-w-7xl mx-auto sm:px-8">
-          <Table
-            headers={
-              <>
-                <Th>Name</Th>
-                <Th>Manager</Th>
-                <Th>Description</Th>
-                <ThSr>View</ThSr>
-              </>
-            }
-          >
-            {accessPoints.map((i) => (
-              <tr key={i.id}>
-                <TdProminent>{i.name}</TdProminent>
-                <Td>{i.accessManager.name}</Td>
-                <Td>{i.description}</Td>
-                <TdLink to={`${i.id}`}>View</TdLink>
-              </tr>
-            ))}
-          </Table>
-        </div>
-      </main>
+      <Header title="Points" />
+      <Main>
+        <Table
+          headers={
+            <>
+              <Th>Name</Th>
+              <Th>Manager</Th>
+              <Th>Description</Th>
+              <ThSr>View</ThSr>
+            </>
+          }
+        >
+          {accessPoints.map((i) => (
+            <tr key={i.id}>
+              <TdProminent>{i.name}</TdProminent>
+              <Td>{i.accessManager.name}</Td>
+              <Td>{i.description}</Td>
+              <TdLink to={`${i.id}`}>View</TdLink>
+            </tr>
+          ))}
+        </Table>
+      </Main>
     </>
   );
 }
