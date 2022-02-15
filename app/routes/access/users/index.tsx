20ef5a27cfcd51483d48ef8d2b4dae310803561b
Michael Wu <mw10013@gmail.com>
2022-02-14 22:48:07 -0500

    code and activate expire status

--- app/routes/access/users/index.tsx
+++ app/routes/access/users/index.tsx
@@ -19,6 +19,39 @@ type LoaderData = {
   accessUsers: Prisma.AccessUserGetPayload<{}>[];
 };
 
+function codeActivateExpireStatus(
+  accessUser: LoaderData["accessUsers"][number]
+) {
+  // JSON serializes dates as strings. The dates in LoaderData will come out as strings on the client.
+  const activateCodeAt = accessUser.activateCodeAt
+    ? new Date(accessUser.activateCodeAt)
+    : null;
+  const expireCodeAt = accessUser.expireCodeAt
+    ? new Date(accessUser.expireCodeAt)
+    : null;
+  const now = Date.now();
+
+  const codeStatus =
+    expireCodeAt && now > expireCodeAt.getTime()
+      ? "EXPIRED"
+      : activateCodeAt && now < activateCodeAt.getTime()
+      ? "PENDING"
+      : "ACTIVE";
+
+  const activateExpireStatus =
+    codeStatus === "ACTIVE"
+      ? expireCodeAt
+        ? `Will expire at ${expireCodeAt.toLocaleString()}`
+        : ``
+      : codeStatus === "PENDING"
+      ? expireCodeAt
+        ? `Will activate at ${activateCodeAt?.toLocaleString()} until ${expireCodeAt.toLocaleString()}.`
+        : `Will activate at ${activateCodeAt?.toLocaleString()}`
+      : ``;
+
+  return { codeStatus, activateExpireStatus };
+}
+
 export const loader: LoaderFunction = async ({
   request,
 }): Promise<LoaderData> => {
@@ -47,18 +80,26 @@ export default function RouteComponent() {
               <Th>Name</Th>
               <Th>Id</Th>
               <Th>Code</Th>
+              <Th>Code Status</Th>
+              <Th>Activate Expire Status</Th>
               <ThSr>View</ThSr>
             </>
           }
         >
-          {accessUsers.map((i) => (
-            <tr key={i.id}>
-              <TdProminent>{i.name}</TdProminent>
-              <Td>{i.id}</Td>
-              <Td>{i.code}</Td>
-              <TdLink to={i.id.toString()}>View</TdLink>
-            </tr>
-          ))}
+          {accessUsers.map((i) => {
+            const { codeStatus, activateExpireStatus } =
+              codeActivateExpireStatus(i);
+            return (
+              <tr key={i.id}>
+                <TdProminent>{i.name}</TdProminent>
+                <Td>{i.id}</Td>
+                <Td>{i.code}</Td>
+                <Td>{codeStatus}</Td>
+                <Td>{activateExpireStatus}</Td>
+                <TdLink to={i.id.toString()}>View</TdLink>
+              </tr>
+            );
+          })}
         </Table>
       </Main>
     </>
