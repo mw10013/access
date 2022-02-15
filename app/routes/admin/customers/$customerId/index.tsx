0e6964ed60ce9b15624a33a72da50dcd2caef6ed
Michael Wu <mw10013@gmail.com>
2022-02-15 15:16:27 -0500

    code, activate and expire status

--- app/routes/admin/customers/$customerId/index.tsx
+++ app/routes/admin/customers/$customerId/index.tsx
@@ -51,6 +51,39 @@ export const loader: LoaderFunction = async ({
   return { customer };
 };
 
+function codeActivateExpireStatus(
+  accessUser: LoaderData["customer"]["accessUsers"][number]
+) {
+  // JSON serializes dates as strings. The dates in LoaderData will come out as strings on the client.
+  const activateCodeAt = accessUser.activateCodeAt
+    ? new Date(accessUser.activateCodeAt)
+    : null;
+  const expireCodeAt = accessUser.expireCodeAt
+    ? new Date(accessUser.expireCodeAt)
+    : null;
+  const now = Date.now();
+
+  const codeStatus =
+    expireCodeAt && now > expireCodeAt.getTime()
+      ? "EXPIRED"
+      : activateCodeAt && now < activateCodeAt.getTime()
+      ? "PENDING"
+      : "ACTIVE";
+
+  const activateExpireStatus =
+    codeStatus === "ACTIVE"
+      ? expireCodeAt
+        ? `Will expire at ${expireCodeAt.toLocaleString()}`
+        : ``
+      : codeStatus === "PENDING"
+      ? expireCodeAt
+        ? `Will activate at ${activateCodeAt?.toLocaleString()} until ${expireCodeAt.toLocaleString()}.`
+        : `Will activate at ${activateCodeAt?.toLocaleString()}`
+      : ``;
+
+  return { codeStatus, activateExpireStatus };
+}
+
 export default function RouteComponent() {
   const { customer } = useLoaderData<LoaderData>();
   const submit = useSubmit();
@@ -104,27 +137,26 @@ export default function RouteComponent() {
                 <Th>Name</Th>
                 <Th>ID</Th>
                 <Th>Code</Th>
-                <Th>Activate Code At</Th>
-                <Th>Expire Code At</Th>
+                <Th>Code Status</Th>
+                <Th>Activate Expire Status</Th>
                 <ThSr>View</ThSr>
               </>
             }
           >
-            {customer.accessUsers.map((i) => (
-              <tr key={i.id}>
-                <TdProminent>{i.name}</TdProminent>
-                <Td>{i.id}</Td>
-                <Td>{i.code}</Td>
-                <Td>
-                  {i.activateCodeAt &&
-                    new Date(i.activateCodeAt).toLocaleString()}
-                </Td>
-                <Td>
-                  {i.expireCodeAt && new Date(i.expireCodeAt).toLocaleString()}
-                </Td>
-                <TdLink to={`users/${i.id}`}>View</TdLink>
-              </tr>
-            ))}
+            {customer.accessUsers.map((i) => {
+              const { codeStatus, activateExpireStatus } =
+                codeActivateExpireStatus(i);
+              return (
+                <tr key={i.id}>
+                  <TdProminent>{i.name}</TdProminent>
+                  <Td>{i.id}</Td>
+                  <Td>{i.code}</Td>
+                  <Td>{codeStatus}</Td>
+                  <Td>{activateExpireStatus}</Td>
+                  <TdLink to={`users/${i.id}`}>View</TdLink>
+                </tr>
+              );
+            })}
           </Table>
         </Card>
       </Main>
